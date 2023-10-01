0dc975730b55a6bc99f10a707eb6243964bf294a
ryudaitakai <ryudai.takai@zozo.com>
2023-10-01 09:54:24 +0900

    Separate js files

---
+++ BoldNumbers/js/boldyfy.js
@@ -0,0 +1,16 @@
+// ボタンがクリックされたら、現在のタブ上のすべての数字を太字にします
+document.getElementById('boldifyButton').addEventListener('click', function() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      func: function() {
+        const numbers = document.querySelectorAll('*'); // ページ上のすべての要素を選択します
+        numbers.forEach(function(element) {
+          if (!isNaN(element.innerText)) {
+            element.style.fontWeight = 'bold';
+          }
+        });
+      }
+    });
+  });
+});
